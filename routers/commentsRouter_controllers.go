6aa7846c41a3f726b89048d4903816d167d5a063
faidercamilo26 <olayafaider@gmail.com>
2024-11-27 07:58:55 -0500

    feature: Se crea la funcion para guardar los resultados de una evaluación a un proveedor. udistrital/Cumplidos_y_certificaciones_documentacion#245

---
+++ routers/commentsRouter_controllers.go
@@ -0,0 +1,28 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+    beego.GlobalControllerRouter["github.com/udistrital/evaluacion_cumplido_prov_mid/controllers:ResultadoEvaluacionController"] = append(beego.GlobalControllerRouter["github.com/udistrital/evaluacion_cumplido_prov_mid/controllers:ResultadoEvaluacionController"],
+        beego.ControllerComments{
+            Method: "GuardarResultadoEvaluacion",
+            Router: "/resultado-evaluacion",
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/udistrital/evaluacion_cumplido_prov_mid/controllers:TestController"] = append(beego.GlobalControllerRouter["github.com/udistrital/evaluacion_cumplido_prov_mid/controllers:TestController"],
+        beego.ControllerComments{
+            Method: "Ping",
+            Router: "/ping",
+            AllowHTTPMethods: []string{"get"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+}
