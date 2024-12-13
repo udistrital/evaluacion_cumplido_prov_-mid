3c9a8a66b06eee981ba6c0844ac014862d749ccb
stevencol <steven2088@hotmail.es>
2024-12-13 05:00:07 -0500

    feat: Modificacion modelo AsignacionEvaluacion

--- models/asignacion_evaluacion.go
+++ models/asignacion_evaluacion.go
@@ -1,13 +1,13 @@
 package models
 
 type AsignacionEvaluacion struct {
-	AsignacionEvaluacionId    int    `json:"AsignacionEvaluacionId"`
-	NombreProveedor           string `json:"NombreProveedor"`
-	Dependencia               string `json:"Depenedencia"`
-	TipoContrato              string `json:"TipoContrato"`
-	NumeroContrato            string `json:"NumeroContrato"`
-	VigenciaContrato          string `json:"VigenciaContrato"`
-	EvaluacionId              int    `json:"EvaluacionId"`
+	AsignacionEvaluacionId    int
+	NombreProveedor           string
+	Dependencia               string
+	TipoContrato              string
+	NumeroContrato            string
+	VigenciaContrato          string
+	EvaluacionId              int
 	EstadoAsignacionEvauacion *EstadoAsignacionEvaluador
 	EstadoEvaluacion          *EstadoEvaluacion
 }
